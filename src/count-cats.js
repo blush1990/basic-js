00e1c692e0428e60d185de350c2e49bbed12f1ac
Evgeny Popov <53173102+blush1990@users.noreply.github.com>
2021-02-01 19:35:17 +0300

    fix: let ind

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,10 +3,10 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(countCats) {
   let res = 0; 
   for (let i=0; i<countCats.length; i++){
-    let index = countCats[i].indexOf('^^');
+    let ind = countCats[i].indexOf('^^');
       if (ind !== -1){
         res += 1;
-        delete countCats[i][index];
+        delete countCats[i][ind];
         i = i - 1;
       }  
   }
