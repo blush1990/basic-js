f24107ab4eff0377920f5d545528fa7c0cca4ca0
blush1990 <evgenypopov16051995@gmail.com>
2021-02-02 22:22:19 +0300

    feat: add task solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  
+if (!Array.isArray(arr)) {
+  return false;
+}
+
+else {
+  let arrdream = [];
+  for(i=0; i<arr.length; i++){
+    if (typeof arr[i] === 'string') {
+      arrdream.push(arr[i].toUpperCase().trim().slice(0, 1)); 
+    }
+  }
+
+  return arrdream.sort().join("");
+}
 };
