20bcc5afd8645b6814917fa50ab60a81b46499c7
Evgeny Popov <53173102+blush1990@users.noreply.github.com>
2021-02-04 04:26:15 -0300

    feat: add task solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)){
+      throw new Error;
+    }
+    let arr2 = [...arr];
+    for (let i=0; i<arr2.length; i++) {
+      if (arr2[i] === '--discard-next') {
+        if (i < (arr2.length-1)){
+        arr2.splice(i+1, 1);
+        }
+      }
+      else if (arr2[i] === '--discard-prev') {
+        if (i > 0) {
+        arr2.splice(i-1, 1);
+        }
+      }
+      else if (arr2[i] === '--double-next') {
+        if (i < (arr2.length-1)) {
+          arr2[i]=arr2[i+1];
+        }
+      }
+      else if (arr2[i] === '--double-prev'){
+        if (i > 0) {
+          arr2[i]=arr2[i-1];
+        } 
+      }        
+    }
+    return arr2.filter(value => {return value !== '--discard-next' && value !== '--discard-prev' && value !== '--double-next' && value !== '--double-prev' });
+  };
